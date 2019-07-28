611521e3f29719c955d2e2e67ee83052cbf4f05d
Hyunju Woo <44936075+DeveloperWoo@users.noreply.github.com>
2019-07-28 16:32:31 -0400

    Add files via upload

---
+++ assets/data/mySongs.ts
@@ -0,0 +1,34 @@
+import {SongInfo} from '../../app/songInfo';
+
+export const MYSONGS: SongInfo[] = [
+    {
+        songName:'If I can’t have you', 
+        artist: 'Shawn Mendes', 
+        genre: 'Pop', 
+        yearReleased: 2019, 
+        picture: 'assets/images/IfIcanthaveyou.jpg'
+    },
+    {
+        songName:'Spirit', 
+        artist: 'Beyonce', 
+        genre: 'Pop', 
+        yearReleased: 2019, 
+        picture: 'assets/images/spirit.jpg'
+    },
+    {
+        songName:'I don\'t care', 
+        artist: 'Sheeran and Justin Bieber', 
+        genre: 'Pop', 
+        yearReleased: 2019, 
+        picture: 'assets/images/Idontcare.jpg'
+    },
+    {
+        songName:'Boy With Luv', 
+        artist: 'BTS', 
+        genre: 'K-pop', 
+        yearReleased: 2019, 
+        picture: 'assets/images/boywithluv.jpg'
+    }
+]
+
+
